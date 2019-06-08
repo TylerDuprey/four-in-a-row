3d33e1fa7e1202df3754025e5c919d8322fad622
Tyler Duprey <tylerduprey@outlook.com>
2019-06-08 15:00:51 -0400

    Add basic JavaScript classes and methods

---
+++ js/Space.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+class Space {
+	constructor(x,y,id) {
+		this.x = x;
+		this.y = y;
+		this.id = `space-${x}-${y}`;
+		this.token = null;
+	}
+}
---
+++ js/Token.js
@@ -0,0 +1,8 @@
+// JavaScript Document
+class Token {
+	constructor(index,owner) {
+		this.owner = owner;
+		this.id = `token-${index}-${owner.id}`;
+		this.dropped = false;
+	}	
+}
